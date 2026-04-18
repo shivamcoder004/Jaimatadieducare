360c26e9f33258b737d08896617bb012f4323bc7
shivamcoder004 <testyug5454@gmail.com>
2026-04-18 09:48:04 +0530

    change number

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         {children}
         {/* --- Floating WhatsApp Button --- */}
         <a
-          href="https://wa.me/8252895483" 
+          href="https://wa.me/8507948665" 
           target="_blank"
           rel="noopener noreferrer"
           className="fixed bottom-6 right-6 z-50 bg-[#25D366] p-3 rounded-full shadow-2xl hover:bg-[#128C7E] transition-all duration-300 hover:scale-110 flex items-center justify-center"
