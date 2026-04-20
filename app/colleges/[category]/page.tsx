9871e5a6b17343e83047941a391cbf1f87d080f3
shivamcoder004 <testyug5454@gmail.com>
2026-04-20 17:44:28 +0530

    add no.

--- app/colleges/[category]/page.tsx
+++ app/colleges/[category]/page.tsx
@@ -71,7 +71,7 @@ export default function CategoryPage({
 
     {/* WhatsApp */}
     <a
-      href="https://wa.me/919876543210"
+      href="https://wa.me/6207541303"
       target="_blank"
       className="flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 px-6 py-3 rounded-xl font-semibold shadow-lg hover:scale-105 transition"
     >
@@ -80,10 +80,10 @@ export default function CategoryPage({
     </a>
 
     {/* Explore */}
-    <button className="flex items-center justify-center gap-2 border border-white/40 px-6 py-3 rounded-xl hover:bg-white/10 transition">
+    {/* <button className="flex items-center justify-center gap-2 border border-white/40 px-6 py-3 rounded-xl hover:bg-white/10 transition">
       Explore Colleges
       <ArrowRight className="w-5 h-5" />
-    </button>
+    </button> */}
 
   </div>
 
