aa2ff24b8272fc41ecf19fb6df18b7cfb4047eba
shivamcoder004 <testyug5454@gmail.coom>
2026-04-20 17:56:01 +0530

    error

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,13 +21,14 @@ export default function RootLayout({
         <Navbar />
         {children}
         {/* --- Floating WhatsApp Button --- */}
-        
+
         <a
           href="https://wa.me/6207541303" 
           target="_blank"
           rel="noopener noreferrer"
           className="fixed bottom-6 right-6 z-50 bg-[#25D366] p-3 rounded-full shadow-2xl hover:bg-[#128C7E] transition-all duration-300 hover:scale-110 flex items-center justify-center"
         >
+          
           <svg
             width="35"
             height="35"
