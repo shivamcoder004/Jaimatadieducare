ea3f577b4f9b3c8023c345e30af0d547a8b20c21
shivamcoder004 <testyug5454@gmail.com>
2026-04-20 17:59:26 +0530

    error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           className="fixed bottom-6 right-6 z-50 bg-[#25D366] p-3 rounded-full shadow-2xl hover:bg-[#128C7E] transition-all duration-300 hover:scale-110 flex items-center justify-center"
         >
           
+
           <svg
             width="35"
             height="35"
